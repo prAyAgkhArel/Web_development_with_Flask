cae4b9c9fc8336ce62115be79012c8ede204040a
Prayag Kharel <125990843+prAyAgkhArel@users.noreply.github.com>
2025-02-22 00:09:58 +0545

    Update main.py

--- Advance_Flask_forms/main.py
+++ Advance_Flask_forms/main.py
@@ -4,18 +4,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length  # pip install email-validator
 from flask_bootstrap import Bootstrap5  # pip install bootstrap-flask
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 
 class LoginForm(FlaskForm):
